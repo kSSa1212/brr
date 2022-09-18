515b11c867ee879190c901a1f29a1ab8dc323f9a
kSSa1212 <113550093+kSSa1212@users.noreply.github.com>
2022-09-18 14:18:55 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,226 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+var prefix = "!"; // تعديل مهم جدا
+var statuses = [`sAKU#2915`];
+var timers = 50000;
+const owners = [`999643171724214272`,``,``];//اي دي الاونرات
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+ client.user.setActivity(`sAKU#2915`, { type: 'WATCHING' })  
+  var timeing = Math.floor(timers * 1000);
+  setInterval(function() {
+    var lengthesof = statuses.length;
+    var amounter = Math.floor(Math.random() * lengthesof);
+    client.user.setActivity(statuses[amounter], { type: "" });
+  }, timeing);
+});
+
+
+
+client.on("message", message => {
+  if (message.content.toLowerCase().startsWith(prefix + "help".toLowerCase())) {
+    message.react("✅");
+    let help = new Discord.MessageEmbed()
+      .setTimestamp()
+      .setAuthor(
+        message.author.username,
+        message.author.displayAvatarURL({ dynamic: true })
+      )
+      .setFooter(`Developed by Givara `, client.user.displayAvatarURL())
+      .setThumbnail(client.user.displayAvatarURL())
+.setDescription(`
+> **Public Commands**
+> \`*1\` **${prefix}ping **
+**~~===================================~~**
+> **Admin Commands**
+> \`*1\` **${prefix}obc : \`لارسال مسدج لي الاونلاين\`**
+> \`*2\` **${prefix}bc : \`لارسال مسدج لي الاونلاين\`**
+> \`*3\` **${prefix}setname : \`لتغير اسم البوت\`**
+> \`*4\` **${prefix}setavatar : \`لتغير صورة البوت\`**
+`)  
+
+    message.channel.send(help);
+  }
+});
+
+client.on("message", message => {
+  if (message.content.startsWith(prefix + "bc")) {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`>  \` **You Dont Have a Permissions**\``);
+
+    message.delete();
+    let args = message.content
+      .split(" ")
+      .slice(1)
+      .join(" ");
+    let noargs = new Discord.MessageEmbed()
+      .setAuthor(
+        message.author.username,
+        message.author.displayAvatarURL({ dynamic: true })
+      )
+      .addField(`Error :x:`, `Please type your broadcast message !`)
+      .setTimestamp()
+      .setFooter(
+        message.author.username,
+        message.author.displayAvatarURL({ dynamic: true })
+      );
+    if (!args) return message.channel.send(message.author, noargs);
+    message.guild.members.cache
+      .filter(m => m.presence.status !== "online")
+      .forEach(m => {
+        if (m.user.bot) return;
+        m.send(`${args}\n ${m}`)
+          .then(() => {
+            console.log(`I Could Send To : ${m.user.tag} ✅`);
+          })
+          .catch(function() {
+            console.log(`I Couldn't Send To : ${m.user.tag} ❌ `);
+          });
+      });
+    let embed = new Discord.MessageEmbed()
+      .setAuthor(
+        message.author.username,
+        message.author.displayAvatarURL({ dynamic: true })
+      )
+      .setDescription(
+        `📬 : تـم ارسـال رسـالـتـك الـى : \`${
+          message.guild.members.cache.filter(
+            m => m.presence.status !== "online"
+          ).size
+        }\` `
+      )
+      .setTimestamp()
+      .setFooter(
+        message.author.username,
+        message.author.displayAvatarURL({ dynamic: true })
+      );
+    message.channel
+      .send(`جـاري الارسال الرساله الى جميع الاعضاء ..`)
+      .then(me => {
+        me.edit(message.author, embed);
+      });
+  }
+});
+
+client.on("message", message => {
+  if (message.content.startsWith(prefix + "obc")) {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`انت لست من الادمن`);
+
+    message.delete();
+    let args = message.content
+      .split(" ")
+      .slice(1)
+      .join(" ");
+    let noargs = new Discord.MessageEmbed()
+      .setAuthor(
+        message.author.username,
+        message.author.displayAvatarURL({ dynamic: true })
+      )
+      .addField(`Error :x:`, `Please type your broadcast message !`)
+      .setTimestamp()
+      .setFooter(
+        message.author.username,
+        message.author.displayAvatarURL({ dynamic: true })
+      );
+    if (!args) return message.channel.send(message.author, noargs);
+    message.guild.members.cache
+      .filter(m => m.presence.status !== "offline")
+      .forEach(m => {
+        if (m.user.bot) return;
+        m.send(`${args}\n ${m}`)
+          .then(() => {
+            console.log(`I Could Send To : ${m.user.tag} ✅`);
+          })
+          .catch(function() {
+            console.log(`I Couldn't Send To : ${m.user.tag} ❌ `);
+          });
+      });
+    let embed = new Discord.MessageEmbed()
+      .setAuthor(
+        message.author.username,
+        message.author.displayAvatarURL({ dynamic: true })
+      )
+      .setDescription(
+        `📬 : تـم ارسـال رسـالـتـك الـى : \`${
+          message.guild.members.cache.filter(
+            m => m.presence.status !== "offline"
+          ).size
+        }\` `
+      )
+      .setTimestamp()
+      .setFooter(
+        message.author.username,
+        message.author.displayAvatarURL({ dynamic: true })
+      );
+    message.channel
+      .send(`جـاري الارسال الرساله الى جميع الاعضاء ..`)
+      .then(me => {
+        me.edit(message.author, embed);
+      });
+  }
+});
+
+client.on("message", async message => {
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("Pinging..").then(m => {
+      m.edit(
+        `\`\`\`javascript\nDiscord API : ${Math.round(
+          client.ws.ping
+        )} ms\n\`\`\` `
+      );
+    });
+  }
+   
+    
+  
+});
+
+client.on("message", message => {
+  if (message.content.startsWith(prefix + "setname")) {
+    let args = message.content.split(" ");
+    let botnameee = args.slice(1).join(" ");
+    if (!owners.includes(message.author.id))
+      return message.channel.send(
+        `** :x: Only Owners Can   Use this Command **`
+      );
+    if (!botnameee)
+      return message.channel.send(
+        `** :x: Please Provide me a name for the bot !**`
+      );
+    client.user.setUsername(`${botnameee}`);
+    message.channel.send(`Changing The bot's Name ...`).then(me => {
+      me.edit(` Done !`);
+    });
+  }
+  if (message.content.startsWith(prefix + "setavatar")) {
+    let args = message.content.split(" ");
+    let botnameee = args.slice(1).join(" ");
+    if (!owners.includes(message.author.id))
+      return message.channel.send(
+        `** :x: Only Owners Can   Use this Command **`
+      );
+    if (!botnameee)
+      return message.channel.send(
+        `** :x: Please Provide me an avatar for the bot !**`
+      );
+    client.user.setAvatar(`${botnameee}`);
+    message.channel.send(`Changing The bot's Avatar ...`).then(me => {
+      me.edit(` Done !`);
+    });
+  }
+});
+
+
+client.login(process.env.token);
